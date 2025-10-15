f2a1ce5bd88ba316b815f27a985af975d0112df9
Steven Saporta <sasaporta@gmail.com>
2025-10-15 14:17:11 -0400

    Added curved road snap test cases

--- cypress/e2e/snap2.cy.js
+++ cypress/e2e/snap2.cy.js
@@ -4,16 +4,23 @@ const tolerance = 1;
 
 const snapCases = [];
 for (const lanes of [2, 4]) {
+
     snapCases.push(
         {
             name: 'straight, horizontal, snap to right edge',
-            componentType: 'straight-road',
-            orientation: 'horizontal',
             lanes: lanes,
-            dropA_X: 300, 
-            dropA_Y: 300,
-            dropB_X: 452,
-            dropB_Y: 302,
+            roadA: {
+                componentType: 'straight-road',
+                orientation: 'horizontal',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'straight-road',
+                orientation: 'horizontal',
+                dropX: 452,
+                dropY: 302,
+            },
             expectedLeft: 375, 
             expectedTop: lanes == 2 ? 275 : 250 
         }
@@ -21,13 +28,19 @@ for (const lanes of [2, 4]) {
     snapCases.push(
         {
             name: 'straight, horizontal, snap to left edge',
-            componentType: 'straight-road',
-            orientation: 'horizontal',
             lanes: lanes,
-            dropA_X: 300, 
-            dropA_Y: 300,
-            dropB_X: 148,
-            dropB_Y: 302,
+            roadA: {
+                componentType: 'straight-road',
+                orientation: 'horizontal',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'straight-road',
+                orientation: 'horizontal',
+                dropX: 148,
+                dropY: 302,
+            },
             expectedLeft: 75,
             expectedTop: lanes == 2 ? 275 : 250 
         }
@@ -35,13 +48,21 @@ for (const lanes of [2, 4]) {
     snapCases.push(
         {
             name: 'straight, vertical, snap to bottom edge',
-            componentType: 'straight-road',
-            orientation: 'vertical',
             lanes: lanes,
-            dropA_X: 300, 
-            dropA_Y: 300,
-            dropB_X: 302,
-            dropB_Y: 452,
+            roadA: {
+                componentType: 'straight-road',
+                orientation: 'vertical',
+                dropX: 300, 
+                dropY: 300,
+                dropB_X: 302,
+                dropB_Y: 452,
+            },
+            roadB: {
+                componentType: 'straight-road',
+                orientation: 'vertical',
+                dropX: 302,
+                dropY: 452,
+            },
             expectedLeft: lanes == 2 ? 275: 250,
             expectedTop: 375 
         },
@@ -49,15 +70,102 @@ for (const lanes of [2, 4]) {
     snapCases.push(
         {
             name: 'straight, vertical, snap to top edge',
-            componentType: 'straight-road',
-            orientation: 'vertical',
             lanes: lanes,
-            dropA_X: 300, 
-            dropA_Y: 300,
-            dropB_X: 302,
-            dropB_Y: 152,
+            roadA: {
+                componentType: 'straight-road',
+                orientation: 'vertical',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'straight-road',
+                orientation: 'vertical',
+                dropX: 302,
+                dropY: 152,
+            },
             expectedLeft: lanes == 2 ? 275: 250,
             expectedTop: 75 
+        },
+    );
+
+    snapCases.push(
+        {
+            name: 'curved, snap to top right',
+            lanes: lanes,
+            roadA: {
+                componentType: 'curved-road',
+                orientation: '0',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'curved-road',
+                orientation: '90',
+                dropX: 452,
+                dropY: 302,
+            },
+            expectedLeft: 375,
+            expectedTop: 225
+        },
+    );
+    snapCases.push(
+        {
+            name: 'curved, snap to bottom right',
+            lanes: lanes,
+            roadA: {
+                componentType: 'curved-road',
+                orientation: '90',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'curved-road',
+                orientation: '180',
+                dropX: 302,
+                dropY: 452,
+            },
+            expectedLeft: 225,
+            expectedTop: 375
+        },
+    );
+    snapCases.push(
+        {
+            name: 'curved, snap to bottom left',
+            lanes: lanes,
+            roadA: {
+                componentType: 'curved-road',
+                orientation: '180',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'curved-road',
+                orientation: '270',
+                dropX: 152,
+                dropY: 302,
+            },
+            expectedLeft: 75,
+            expectedTop: 225
+        },
+    );
+    snapCases.push(
+        {
+            name: 'curved, snap to top left',
+            lanes: lanes,
+            roadA: {
+                componentType: 'curved-road',
+                orientation: '270',
+                dropX: 300, 
+                dropY: 300,
+            },
+            roadB: {
+                componentType: 'curved-road',
+                orientation: '0',
+                dropX: 302,
+                dropY: 152,
+            },
+            expectedLeft: 225,
+            expectedTop: 75
         },
     );
 }
@@ -73,19 +181,19 @@ function verifySnapBehavior(testCase) {
         const canvasClientTop = rect.top;
 
         // Calculate absolute client coordinates for Drop A and Drop B
-        clientA_X = canvasClientLeft + testCase.dropA_X;
-        clientA_Y = canvasClientTop + testCase.dropA_Y;
-        clientB_X = canvasClientLeft + testCase.dropB_X;
-        clientB_Y = canvasClientTop + testCase.dropB_Y;
+        clientA_X = canvasClientLeft + testCase.roadA.dropX;
+        clientA_Y = canvasClientTop + testCase.roadA.dropY;
+        clientB_X = canvasClientLeft + testCase.roadB.dropX;
+        clientB_Y = canvasClientTop + testCase.roadB.dropY;
 
         // B. DROP ROAD A (The Base Road)
-        cy.log(`Drop Road A (${testCase.name}): Internal X: ${testCase.dropA_X}, Y: ${testCase.dropA_Y}`);
+        cy.log(`Drop Road A (${testCase.name}): Internal X: ${testCase.roadA.dropX}, Y: ${testCase.roadA.dropY}`);
         cy.get('#diagram-canvas').trigger('drop', {
             clientX: clientA_X, 
             clientY: clientA_Y,
-            componentType: testCase.componentType,
-            orientation: testCase.orientation,
-            lanes: testCase.lanes, 
+            componentType: testCase.roadA.componentType,
+            orientation: testCase.roadA.orientation,
+            lanes: testCase.lanes,
         });
     });
 
@@ -96,12 +204,12 @@ function verifySnapBehavior(testCase) {
 
     // C. DROP ROAD B (The Test Road - expecting snap)
     cy.get('#diagram-canvas').then(() => {
-        cy.log(`Drop Road B (${testCase.name}): Internal X: ${testCase.dropB_X}, Y: ${testCase.dropB_Y}`);
+        cy.log(`Drop Road B (${testCase.name}): Internal X: ${testCase.roadB.dropX}, Y: ${testCase.roadB.dropY}`);
         cy.get('#diagram-canvas').trigger('drop', {
             clientX: clientB_X, 
             clientY: clientB_Y,
-            componentType: testCase.componentType,
-            orientation: testCase.orientation,
+            componentType: testCase.roadB.componentType,
+            orientation: testCase.roadB.orientation,
             lanes: testCase.lanes, 
         });
     });
